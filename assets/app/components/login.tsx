9d145061fda40a966a30b4a9c8677319de6e688a
tiana <tiana.randrianarivony@gmail.com>
2023-05-02 09:46:09 +0300

    setting prettier config

--- assets/app/components/login.tsx
+++ assets/app/components/login.tsx
@@ -1,81 +1,89 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, { ChangeEvent, useState } from 'react'
 import {
-    Typography,
-    TextField,
-    FormControlLabel,
-    Checkbox,
-    Button,
-    Link,
-    Grid,
-} from '@material-ui/core';
-import axios from 'axios';
+  Typography,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Link,
+  Grid,
+} from '@material-ui/core'
+import axios from 'axios'
 
 const Login = () => {
-    const [creds, setCredentials] = useState<{email: string, password: string}>({email: 'demo@dev.mg', password: ''})
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        console.log(creds);
-        const credentialRoute = `${process.env.API_URL}/authentication`
-        const {data} = await axios.post(credentialRoute, creds).catch(e => e.response)
+  const [creds, setCredentials] = useState<{ email: string; password: string }>(
+    { email: 'demo@dev.mg', password: '' }
+  )
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const credentialRoute = `${process.env.API_URL}/authentication`
+    const { data } = await axios
+      .post(credentialRoute, creds)
+      .catch((e) => e.response)
+  }
 
-    };
+  const handleFieldChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name } = e.target
+    setCredentials({ ...creds, [name]: e.target.value })
+  }
 
-    const handleFieldChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const {name} = e.target
-        setCredentials({...creds, [name]: e.target.value})
-    }
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: 400 }}>
+        <Typography variant="h4" align="center" gutterBottom>
+          Manager login
+        </Typography>
+        <TextField
+          value={creds.email}
+          onChange={handleFieldChange}
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          id="email"
+          label="Email address"
+          name="email"
+          autoComplete="off"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          value={creds.password}
+          onChange={handleFieldChange}
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
 
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: 400 }}>
-                <Typography variant="h4" align="center" gutterBottom>
-                    Manager login
-                </Typography>
-                <TextField
-                    value={creds.email}
-                    onChange={handleFieldChange}
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    id="email"
-                    label="Email address"
-                    name="email"
-                    autoComplete="off"
-                    autoFocus
-                />
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    value={creds.password}
-                    onChange={handleFieldChange}
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          style={{ marginTop: '1rem' }}
+        >
+          Login
+        </Button>
+        <Grid container style={{ marginTop: '1rem' }} justifyContent={'center'}>
+          <Grid item>
+            <Link href="#" variant="body2">
+              New account ?
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  )
+}
 
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    style={{ marginTop: '1rem' }}
-                >
-                    Login
-                </Button>
-                <Grid container style={{ marginTop: '1rem' }}  justifyContent={'center'}>
-
-                    <Grid item>
-                        <Link href="#" variant="body2">
-                            New account ?
-                        </Link>
-                    </Grid>
-                </Grid>
-            </form>
-        </div>
-    );
-};
-
-export default Login;
+export default Login
