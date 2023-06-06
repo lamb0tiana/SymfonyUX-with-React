d591c90277f74f18627479bb4244b1a6ab94e179
tiana <tiana.randrianarivony@gmail.com>
2023-06-06 16:25:25 +0300

    playercollection query #3, resolve duplicated named type

---
+++ PlayerItemType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\GraphQl\Types;
+
+use ApiPlatform\GraphQl\Type\Definition\TypeInterface;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Utils\BuildSchema;
+
+class PlayerItemType extends ObjectType implements TypeInterface
+{
+    public function __construct(private string $dir)
+    {
+        $out = file_get_contents("$dir/public/schema.graphql");
+        $schema = BuildSchema::build($out);
+        $type =  $schema->getType('Player');
+        $fields = $type->getFields();
+        unset($fields['playerTeams']);
+
+        $config = [
+            'fields' => $fields,
+
+        ];
+        parent::__construct($config);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+}
--- src/GraphQl/Types/PlayerCollection.php
+++ src/GraphQl/Types/PlayerCollection.php
@@ -4,21 +4,20 @@ namespace App\GraphQl\Types;
 
 use ApiPlatform\GraphQl\Type\Definition\TypeInterface;
 use App\Repository\PlayerRepository;
-use Doctrine\Common\Collections\ArrayCollection;
 use GraphQL\Type\Definition\ListOfType;
 use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+
 
 class PlayerCollection extends ObjectType implements TypeInterface
 {
-    public function __construct(private PlayerRepository $repository, private PlayerItemType $itemType, private PlayerRepository $playerRepository)
+    public function __construct(private PlayerItemType $itemType, private PlayerRepository $playerRepository)
     {
 
         $config = [
             'fields' => [
-                'players' => ['type' => ListOfType::listOf($this->itemType),             'resolve' => function ($a) {
-                    return [['name' => 'ok', 'slug' => 'ok']];
-                    $a = new ArrayCollection([$this->playerRepository->find(2)]);
-                    return $a;
+                'players' => ['type' => Type::listOf($this->itemType),            'resolve' => function ($a) {
+                    return [['name' => 'ok', 'id' => 2]];
                 }]
             ],
         ];
